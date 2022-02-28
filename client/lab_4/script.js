3300ba613f666134425f5bb92bf0cecc7c95f136
Ian Winslow <41134386+EeyanW@users.noreply.github.com>
2022-02-27 22:54:34 -0500

    Lab 5
    Lab 5

---
+++ client/lab_4/script.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-restricted-syntax */
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel_item');
+const totalSlides = slides.length;
+
+function updateSlidePosition() {
+  // eslint-disable-next-line prefer-const
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+  slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+  document.querySelector('#carousel_button--next').style.background = '#FFA500';
+  document.querySelector('#carousel_button--prev').style.background = '#FFFFFF';
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
+  document.querySelector('#carousel_button--prev').style.background = '#FFA500';
+  document.querySelector('#carousel_button--next').style.background = '#FFFFFF';
+  updateSlidePosition();
+}
+
+document
+  .querySelector('#carousel_button--next')
+  .addEventListener('click', moveToNextSlide);
+
+document
+  .querySelector('#carousel_button--prev')
+  .addEventListener('click', moveToPrevSlide);
